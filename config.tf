19d7bb905ff22c9b6eb2e9971b6be36976b7d951
Demo User <demouser@dcloud.cisco.com>
2023-12-11 07:21:54 +0000

    Addig terraform state and necessary files

--- config.tf
+++ config.tf
@@ -1,3 +1,12 @@
+terraform {
+  cloud {
+    organization = "ACITest"
+
+    workspaces {
+      name = "aci-demo"
+    }
+  }
+}
 
 resource "aci_tenant" "aci_demo" {
   name        = "aci_demo"    
